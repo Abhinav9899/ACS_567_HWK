67d3886e0180e0422a78b71bf0dca8977b264d3b
Abhinav1919 <kapaa01@pfw.edu>
2024-03-03 21:29:02 -0500

    Pushing the code for Feature A Subtask 1

---
+++ HWK6/src/com/spm/sprint/hw6/TeamCapacityCalculator_FeatureB.java
@@ -0,0 +1,14 @@
+package com.spm.sprint.hw6;
+
+public class TeamCapacityCalculator_FeatureB {
+
+	public TeamCapacityCalculator_FeatureB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HWK6/src/com/spm/sprint/hw6/TeamMember_FeatureB.java
@@ -0,0 +1,9 @@
+package com.spm.sprint.hw6;
+
+public class TeamMember_FeatureB {
+
+	public TeamMember_FeatureB() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
